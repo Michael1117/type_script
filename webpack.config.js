68f55abb586ba3ca893c99f5d47c2a4d89c9a5ff
Michael1117 <1322720377@qq.com>
2019-03-28 10:57:13 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.tsx',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
